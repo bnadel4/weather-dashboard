e7161475ff7b72ce27dd2ff78f10712b35ab29f5
Jamie Nadel <jamienadel4@gmail.com>
2023-03-06 07:44:13 -0500

    adding a get current weather api call

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,27 @@ searchButton.addEventListener('click', function() {
   getCoordinates(userCity);
 });
 
+function getCurrent(data) {
+  console.log('data inside getCurrent', data);
+  var lat = data[0].lat;
+  var lon = data[0].lon;
+  var requestCityCurrent = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+
+  fetch(requestCityCurrent)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('current temp', data.main.temp);
+      // console.log('current icon', data.weather.icon, "http://openweathermap.org/img/wn/${data.weather.icon}@2x.png");
+      console.log('current wind', data.wind.speed);
+      console.log('current humidity', data.main.humidity);
+
+      var currentTempEL = document.createElement('p');
+      currentTempEL.textContent = data.main.temp;
+      currentWeatherEL.appendChild(currentTempEL);
+    });
+}
 
 function getForecast(data) {
   console.log('data inside getForecast', data);
